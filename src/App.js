7e90fa34b73df2ba8629fcbc7f8b9c24a327aee8
Unknown <timofteca@gmail.com>
2019-07-16 12:48:25 +0300

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import React from "react";
 import "./App.css";
 import "antd/dist/antd.css";
-//import Navbar from "./app/Navbar";
 import { Route, Switch } from "react-router-dom";
 import RegistrationForm from "./app/components/auth/Register";
 import LoginForm from "./app/components/auth/Login";
 import Home from "./app/components/Home";
+import Navbar from "./app/components/Navbar";
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <Switch>
         <Route exact path="/" component={Home} />
         <Route path="/login" component={LoginForm} />
